b37e404cb269b4282a6d825d5796426254d0055c
jt@kckustomac.io <jt@kckustomac.io>
2022-07-28 06:28:16 -0500

    220728

---
+++ app/res/dimensions.js
@@ -0,0 +1,7 @@
+import {Dimensions} from 'react-native';
+const dWidth = Dimensions.get('window').width;
+const dHeight = Dimensions.get('window').height;
+export const dimensions = {
+  width: dWidth,
+  height: dHeight,
+};
---
+++ app/src/R.js
@@ -0,0 +1,7 @@
+import {colors} from '../res/colors'
+import {fonts} from '../res/fonts'
+import {images} from '../res/images'
+import {strings} from '../res/strings'
+import {dimensions} from '../res/dimensions.js'
+const R = {colors,fonts,images,strings,dimensions};
+export default R;
